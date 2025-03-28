8af08f644c30c3002a26c4437267b54915f8574d
Manish Kumar Prasad <manishraz32@gmail.com>
2025-03-28 23:28:39 +0530

    inheritance is added

--- JS-OOPS/index.js
+++ JS-OOPS/index.js
@@ -134,4 +134,34 @@
   let user = new User("John");
   console.log("user: ", user);
   user.sayHi();
+
+  // inheritance in class
+  class Animal {
+    constructor(name) {
+      this.speed = 0;
+      this.name = name;
+    }
+    run(speed) {
+      this.speed = speed;
+      alert(`${this.name} runs with speed ${this.speed}.`);
+    }
+    stop() {
+      this.speed = 0;
+      alert(`${this.name} stands still.`);
+    }
+  }
+  
+  let animal = new Animal("My animal");
+
+
+  class Rabbit extends Animal {
+    hide() {
+      alert(`${this.name} hides!`);
+    }
+  }
+  
+  let rabbit = new Rabbit("White Rabbit");
+  
+  rabbit.run(5); // White Rabbit runs with speed 5.
+  rabbit.hide(); // White Rabbit hides!
   
